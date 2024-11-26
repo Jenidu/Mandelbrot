870cd202fc94323ebb72361a51a7dcf9c58f867f
jolyeline <164340137+jolyeline@users.noreply.github.com>
2024-11-26 16:21:18 +0100

    labels maken

--- App/Mandelbrot/Program.cs
+++ App/Mandelbrot/Program.cs
@@ -1,25 +1,33 @@
 ﻿using System.Drawing;
 using System.Windows.Forms;
 
-internal class Mandelbrot_2 : Form
-{
-    public Mandelbrot_2()
-    {
-        this.Text = "Mandelbrot_2";
-        this.BackColor = Color.LightYellow;
-        this.ClientSize = new Size(200, 100);
-        this.Paint += this.Teken;
-    }
-    public void Teken(object o, PaintEventArgs pea)
-    {
-        Graphics gr = pea.Graphics;
-        gr.FillEllipse(Brushes.Blue, 20, 20, 50, 50);
-    }
-    public static void Main()
-    {
-        Application.Run(new Mandelbrot_2());
-    }
-}
+Form scherm = new Form();
+scherm.Text = "Mandelbrot";
+scherm.BackColor = Color.Beige;
+scherm.ClientSize = new Size(300, 400);
+
+Label tekst_midden_x = new Label();  
+Label tekst_midden_y = new Label();
+Label tekst_schaal = new Label();
+Label tekst_max_aantal = new Label();
+TextBox invoer_midden_x = new TextBox();
+TextBox invoer_midden_y = new TextBox();
+TextBox invoer_schaal = new TextBox();
+TextBox invoer_max_aantal = new TextBox();
+Button ga_knop = new Button();
+
+scherm.Controls.Add(tekst_midden_x);
+tekst_midden_x.Location = new Point(10, 15);
+tekst_midden_x.Size = new Size(80, 20);
+tekst_midden_x.Text = "midden x:";
+
+
+scherm.Controls.Add(tekst_midden_y);
+tekst_midden_y.Location = new Point(10, 30);
+tekst_midden_y.Size = new Size(80,20);
+tekst_midden_y.Text = "midden y:";
+
+Application.Run(scherm);
 
 
 

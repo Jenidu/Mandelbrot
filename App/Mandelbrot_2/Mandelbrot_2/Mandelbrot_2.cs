83aedf670c65bf081a4d9951ac7c2d1c4bda83bc
jolyeline <164340137+jolyeline@users.noreply.github.com>
2024-11-26 16:00:12 +0100

    Mandelbrot_2

---
+++ App/Mandelbrot_2/Mandelbrot_2/Mandelbrot_2.cs
@@ -0,0 +1,22 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+internal class Mandelbrot_2 : Form
+{
+    public Mandelbrot_2()
+    {
+        this.Text = "Mandelbrot_2";
+        this.BackColor = Color.LightYellow;
+        this.ClientSize = new Size(200, 100);
+        this.Paint += this.Teken;
+    }
+    public void Teken(object o, PaintEventArgs pea)
+    {
+        Graphics gr = pea.Graphics;
+        gr.FillEllipse(Brushes.Blue, 20, 20, 50, 50);
+    }
+    public static void Main()
+    {
+        Application.Run(new Mandelbrot_2());
+    }
+}

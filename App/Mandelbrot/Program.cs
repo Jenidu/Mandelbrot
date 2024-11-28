c1fc59b926585a85eb75602288a1d22e3f6ee1a5
jolyeline <164340137+jolyeline@users.noreply.github.com>
2024-11-28 12:06:36 +0100

    Mandelgetal

--- App/Mandelbrot/Program.cs
+++ App/Mandelbrot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 Form scherm = new Form();
@@ -17,8 +18,21 @@ TextBox invoer_midden_y = new TextBox();
 TextBox invoer_schaal = new TextBox();
 TextBox invoer_max_aantal = new TextBox();
 Button ga_knop = new Button();
+Bitmap plaatje = new Bitmap(280, 280);
+Label label_plaatje = new Label();
+scherm.Controls.Add(label_plaatje);
+label_plaatje.Location = new Point(15, 130);
+label_plaatje.Size = new Size(270, 260);
+label_plaatje.BackColor = Color.White;
+label_plaatje.Image = plaatje;
+Graphics gr_plaatje = Graphics.FromImage(plaatje);
+
+
+
 
 Opstart_venster();
+
+Teken_figuur();
 
 Application.Run(scherm);
 
@@ -42,10 +56,10 @@ void Opstart_venster(){
     invoer_midden_y.Location = new Point(100, 40);
     invoer_midden_y.Size = new Size(80, 15);
 
-scherm.Controls.Add(tekst_schaal);
-tekst_schaal.Location = new Point(10, 65);
-tekst_schaal.Size = new Size(80, 15);
-tekst_schaal.Text = "schaal";
+    scherm.Controls.Add(tekst_schaal);
+    tekst_schaal.Location = new Point(10, 65);
+    tekst_schaal.Size = new Size(80, 15);
+    tekst_schaal.Text = "schaal";
 
     scherm.Controls.Add(invoer_schaal);
     invoer_schaal.Location = new Point(100, 65);
@@ -72,4 +86,36 @@ double[] functie_f(double x, double y, double a, double b){
     ret[0] = a * a - b * b + x;
     ret[1] = 2 * a * b + y;
     return ret;
+}
+
+
+bool mandelgetal(double x, double y)
+{
+    double[] nieuwe_a_b = [x, y];
+    double afstand = 0;
+    int n;
+
+    for (n = 0; afstand < 2; n++)
+    { 
+        nieuwe_a_b = functie_f(x, y, nieuwe_a_b[0], nieuwe_a_b[1]);
+        afstand = Math.Sqrt(Math.Pow(nieuwe_a_b[0], 2) + Math.Pow(nieuwe_a_b[1], 2));
+    }
+   
+    return (n % 2 == 0);
+}
+
+
+
+void Teken_figuur ()
+{
+    // for (int x = 0; x < plaatje.Width ; x++) {
+    //   for (int y = 0; y < plaatje.Height; y++)
+    // {
+    //   plaatje.SetPixel(x,y, Color.White);
+    //}
+    //}
+
+    gr_plaatje.FillEllipse(Brushes.AliceBlue, 100, 100, 100, 100);
+   
+    
 }
